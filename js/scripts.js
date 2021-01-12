ba68f47aea4a13563d41abf0fbb52bdc7614706c
Justin Golden <justingolden21@gmail.com>
2021-01-11 22:28:21 -0800

    setting up basic layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 window.onload = ()=> {
-	console.log('page loaded');
+	document.getElementById('status-input').select();
+
+	// one details open at a time
+	// https://stackoverflow.com/a/36994802/4907950
+	const details = document.querySelectorAll('details');
+	details.forEach(targetDetail=> {
+		targetDetail.addEventListener('click', ()=> {
+			// close details that aren't the target
+			details.forEach(detail => {
+				if(detail !== targetDetail) {
+					detail.removeAttribute('open');
+				}
+			});
+		});
+	});
 };
