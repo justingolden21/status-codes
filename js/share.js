46ac5b96b46164d8ede5f43949c0fc9132d5c4a7
Justin Golden <justingolden21@gmail.com>
2022-11-17 17:10:09 -0800

    Add print btn and functionality

--- js/share.js
+++ js/share.js
@@ -14,4 +14,37 @@ window.addEventListener('load', function () {
 			console.log('Share not supported');
 		}
 	};
+
+	document.getElementById('print-btn').onclick = function () {
+		window.print();
+	};
 });
+
+// open details before print
+// https://stackoverflow.com/a/34158303/4907950
+
+function beforePrint() {
+	document
+		.querySelectorAll('details')
+		.forEach((elm) => elm.setAttribute('open', true));
+}
+function afterPrint() {
+	document
+		.querySelectorAll('details')
+		.forEach((elm) => elm.removeAttribute('open'));
+}
+
+// webkit
+if (window.matchMedia) {
+	window.matchMedia('print').addListener(function (mql) {
+		if (mql.matches) {
+			beforePrint();
+		} else {
+			afterPrint();
+		}
+	});
+}
+
+// IE and FF
+window.onbeforeprint = beforePrint;
+window.onafterprint = afterPrint;
